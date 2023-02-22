f88d40ccde1b3e86310a2df2e52717b0b9a42fbf
amineelaabdi14 <amineelaabdi@gmail.com>
2023-02-22 17:17:24 +0100

    adding some verification

--- app/Http/Controllers/EditProfileController.php
+++ app/Http/Controllers/EditProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 
 class EditProfileController extends Controller
 {
@@ -19,9 +20,12 @@ class EditProfileController extends Controller
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
+        // if(Hash::check($user->password,$req->password)){
+        //     return response()->json(['message' => 'incorrect password'], 404);
+        // }
         $user->name=$req->name;
         $user->email=$req->email;
-        $user->password=$req->password;
+        $user->password=$req->newPassword;
         $user->save();
         return response()->json([
             'status' => true,
