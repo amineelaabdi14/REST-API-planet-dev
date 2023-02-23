a2d5b518dd2dba3fe01e436278954625bcaaca6e
amineelaabdi14 <amineelaabdi@gmail.com>
2023-02-23 09:46:04 +0100

    added the old password check

--- app/Http/Controllers/EditProfileController.php
+++ app/Http/Controllers/EditProfileController.php
@@ -5,14 +5,16 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 
+use Illuminate\Support\Facades\Hash;
+
 class EditProfileController extends Controller
 {
     //
     
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
 
     public function editInfos(Request $req)
     {
@@ -20,12 +22,12 @@ class EditProfileController extends Controller
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
-        // if(Hash::check($user->password,$req->password)){
-        //     return response()->json(['message' => 'incorrect password'], 404);
-        // }
+        if(!Hash::check($req->password,$user->password)){
+            return response()->json(['message' => 'incorrect password'], 404);
+        }
         $user->name=$req->name;
         $user->email=$req->email;
-        $user->password=$req->newPassword;
+        $user->password= Hash::make($req->newPassword);
         $user->save();
         return response()->json([
             'status' => true,
